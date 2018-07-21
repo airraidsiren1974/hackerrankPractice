c974871d6db42d0d27d12677d6ee12acf8c09053
Jackie Martin <airraid.siren1974@gmail.com>
2018-07-20 17:07:50 -0700

    Eucledian Distance
    HackerRank

---
+++ euclidean_distance.sql
@@ -0,0 +1,9 @@
+/* calculate euclidean distance
+
+((a - c)** 2 + (b - c)** 2)^2
+
+*/
+
+SELECT
+    ROUND(SQRT(POWER((MAX(Lat_N) - MIN(Lat_N)), 2) + POWER((MAX(Long_W) - MIN(Long_W)), 2)), 4)
+FROM Station;
