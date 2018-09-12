97186427d779ca38303e75c9c46bec71c1b5a568
Jackie Martin <airraid.siren1974@gmail.com>
2018-09-11 18:22:05 -0700

    hacker rank grades exercise

---
+++ occupations_subs.sql
@@ -0,0 +1,7 @@
+select name,'('||SUBSTR(occupation, 1, 1)||')' 
+from occupations
+order by name asc;
+select 'There are a total of ' ||count(occupation)||' '||lower(occupation)||'s.' 
+from occupations 
+group by occupation
+order by occupation asc;
